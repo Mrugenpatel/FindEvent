b2b7b33d0598801ff0b05569b54e6a4225b09e8e
Yurii Tsymbala <zumbala2013@gmail.com>
2019-02-04 23:29:28 +0200

    Add pods

---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '11.0'
+
+target 'FamillyOrganizer' do
+  use_frameworks!
+
+ # UI
+    pod 'SnapKit'
+
+ # Autogeneration
+    pod 'R.swift'
+
+
+
+end
---
+++ Source/Application/Global/Closures.swift
@@ -0,0 +1,19 @@
+//
+//  Closures.swift
+//  FamillyOrganizer
+//
+//  Created by Yurii Tsymbala on 2/4/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - All
+
+typealias EmptyClosure = () -> Void
+
+
+// MARK: - UI
+// MARK: Controls / Buttons
+
+//typealias DidTouchUpInside = (Button) -> Void
