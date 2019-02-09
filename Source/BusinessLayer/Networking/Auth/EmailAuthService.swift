a391a5a7b8b99e0855643b8be7343a2170f86a01
Yurii Tsymbala <zumbala2013@gmail.com>
2019-02-10 00:56:46 +0200

    Update AuthService

--- Source/BusinessLayer/Networking/Auth/EmailAuthService.swift
+++ Source/BusinessLayer/Networking/Auth/EmailAuthService.swift
@@ -55,117 +55,117 @@ class EmailAuthService: EmailAuthServiceType {
     var currentUserId: String?
 }
 
-class AuthService {
-    private let firebaseAuth = Auth.auth()
-    private let userService: UserService
-    private var currentFirebaseUser: FirebaseAuth.User? {
-        return firebaseAuth.currentUser
-    }
-
-    var currentUserId: String? {
-        guard let firUserId = currentFirebaseUser?.uid else { return nil }
-        return firUserId
-    }
-
-    init(userService: UserService) {
-        self.userService = userService
-    }
-
-    func signIn(
-        withEmail email: String,
-        withPassword password: String,
-        completion: @escaping AuthHandler
-        ) {
-        firebaseAuth.signIn(withEmail: email, password: password) { [weak self] (_, authError) in
-            guard let strongSelf = self else { return }
-            guard let authError = authError else {
-                guard let currentUserId = strongSelf.currentUserId else { return }
-                strongSelf
-                    .userService
-                    .getById(currentUserId)
-                    .subscribe(onNext: { userResult in
-                        switch userResult {
-                        case .success(let user): completion(.success(user))
-                        case .failure: completion(.failure(.failedToGetUser))
-                        }
-                    }).disposed(by: strongSelf.disposeBag)
-                return
-            }
-            let error = AuthServiceError.getError(error: authError)
-            completion(.failure(error))
-        }
-    }
-
-    func signUp(withName name: String,
-                withEmail email: String,
-                withPassword password: String,
-                withUserImg img: UIImage?,
-                completion: @escaping AuthHandler) {
-        firebaseAuth.createUser(withEmail: email, password: password) { (authResult, authError) in
-            guard let authError = authError else {
-                guard let user = authResult?.user else { completion(.failure(.internalInconsistency)); return }
-                self.updateUser(forUser: user, updateName: name, completion: { [unowned self] (result) in
-                    switch result {
-                    case .success:
-                        guard let currentUserId = self.currentUserId else { completion(.failure(.userNotFound)); return }
-                        self.userService.create(
-                            User(id: currentUserId,
-                                 name: name,
-                                 email: email,
-                                 avatarImgURL: ""
-                            ), completion: { (result) in
-                                switch result {
-                                case .success(let user):
-                                    completion(.success(user))
-                                case .failure:
-                                    completion(.failure(.failedToCreateUser))
-                                }
-                        })
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
-                })
-                return
-            }
-
-            completion(.failure(.getError(error: authError)))
-            return
-        }
-    }
-
-    func signOut(
-        completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
-        do {
-            try firebaseAuth.signOut()
-            completion(.success(true))
-        } catch let signOutError as NSError {
-            completion(.failure(.getError(error: signOutError)))
-        }
-    }
-
-    func updatePassword(newPassword: String,
-                        repeatNewPassword: String,
-                        completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
-        if newPassword.isEmpty, repeatNewPassword.isEmpty { completion(.failure(.emptyPassword)); return }
-        if newPassword == repeatNewPassword {
-            currentFirebaseUser?.updatePassword(to: newPassword, completion: { (error) in
-                if let error = error {
-                    completion(.failure(.getError(error: error)))
-                } else {
-                    completion(.success(true))
-                }
-            })
-        } else {completion(.failure(.failedToChangePassword))}
-    }
-
-    private func updateUser(forUser user: FirebaseAuth.User,
-                            updateName name: String,
-                            completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
-        let updatableUser = user.createProfileChangeRequest()
-        updatableUser.displayName = name
-        updatableUser.commitChanges { responseError in
-            guard let error = responseError else { completion(.success(true)); return }
-            completion(.failure(.getError(error: error)))
-        }
-    }
-}
+//class AuthService {
+//    private let firebaseAuth = Auth.auth()
+//    private let userService: UserService
+//    private var currentFirebaseUser: FirebaseAuth.User? {
+//        return firebaseAuth.currentUser
+//    }
+//
+//    var currentUserId: String? {
+//        guard let firUserId = currentFirebaseUser?.uid else { return nil }
+//        return firUserId
+//    }
+//
+//    init(userService: UserService) {
+//        self.userService = userService
+//    }
+//
+//    func signIn(
+//        withEmail email: String,
+//        withPassword password: String,
+//        completion: @escaping AuthHandler
+//        ) {
+//        firebaseAuth.signIn(withEmail: email, password: password) { [weak self] (_, authError) in
+//            guard let strongSelf = self else { return }
+//            guard let authError = authError else {
+//                guard let currentUserId = strongSelf.currentUserId else { return }
+//                strongSelf
+//                    .userService
+//                    .getById(currentUserId)
+//                    .subscribe(onNext: { userResult in
+//                        switch userResult {
+//                        case .success(let user): completion(.success(user))
+//                        case .failure: completion(.failure(.failedToGetUser))
+//                        }
+//                    }).disposed(by: strongSelf.disposeBag)
+//                return
+//            }
+//            let error = AuthServiceError.getError(error: authError)
+//            completion(.failure(error))
+//        }
+//    }
+//
+//    func signUp(withName name: String,
+//                withEmail email: String,
+//                withPassword password: String,
+//                withUserImg img: UIImage?,
+//                completion: @escaping AuthHandler) {
+//        firebaseAuth.createUser(withEmail: email, password: password) { (authResult, authError) in
+//            guard let authError = authError else {
+//                guard let user = authResult?.user else { completion(.failure(.internalInconsistency)); return }
+//                self.updateUser(forUser: user, updateName: name, completion: { [unowned self] (result) in
+//                    switch result {
+//                    case .success:
+//                        guard let currentUserId = self.currentUserId else { completion(.failure(.userNotFound)); return }
+//                        self.userService.create(
+//                            User(id: currentUserId,
+//                                 name: name,
+//                                 email: email,
+//                                 avatarImgURL: ""
+//                            ), completion: { (result) in
+//                                switch result {
+//                                case .success(let user):
+//                                    completion(.success(user))
+//                                case .failure:
+//                                    completion(.failure(.failedToCreateUser))
+//                                }
+//                        })
+//                    case .failure(let error):
+//                        completion(.failure(error))
+//                    }
+//                })
+//                return
+//            }
+//
+//            completion(.failure(.getError(error: authError)))
+//            return
+//        }
+//    }
+//
+//    func signOut(
+//        completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
+//        do {
+//            try firebaseAuth.signOut()
+//            completion(.success(true))
+//        } catch let signOutError as NSError {
+//            completion(.failure(.getError(error: signOutError)))
+//        }
+//    }
+//
+//    func updatePassword(newPassword: String,
+//                        repeatNewPassword: String,
+//                        completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
+//        if newPassword.isEmpty, repeatNewPassword.isEmpty { completion(.failure(.emptyPassword)); return }
+//        if newPassword == repeatNewPassword {
+//            currentFirebaseUser?.updatePassword(to: newPassword, completion: { (error) in
+//                if let error = error {
+//                    completion(.failure(.getError(error: error)))
+//                } else {
+//                    completion(.success(true))
+//                }
+//            })
+//        } else {completion(.failure(.failedToChangePassword))}
+//    }
+//
+//    private func updateUser(forUser user: FirebaseAuth.User,
+//                            updateName name: String,
+//                            completion: @escaping (Result<Bool, AuthServiceError>) -> Void) {
+//        let updatableUser = user.createProfileChangeRequest()
+//        updatableUser.displayName = name
+//        updatableUser.commitChanges { responseError in
+//            guard let error = responseError else { completion(.success(true)); return }
+//            completion(.failure(.getError(error: error)))
+//        }
+//    }
+//}
--- Source/BusinessLayer/Networking/User/UserService.swift
+++ Source/BusinessLayer/Networking/User/UserService.swift
@@ -71,8 +71,7 @@ class UserService: UserServiceType {
         completion: @escaping (Result<User, UserServiceError>) -> Void
         ) {
         guard let userId = user.id else { completion(.failure(.emptyId)); return }
-        let userRef = userCollection.document(userId)
-        userRef.updateData(user.data()) { responseError in
+        userCollection.document(userId).updateData(user.data()) { responseError in
             guard let firebaseError = responseError else { completion(.success(user)); return }
             let error = UserServiceError.getError(from: firebaseError)
             completion(.failure(error))
@@ -83,37 +82,27 @@ class UserService: UserServiceType {
         userId: String,
         completion: @escaping (Result<User, UserServiceError>) -> Void
         ) {
-        //        return Observable<UserResult>.create({ [weak self] observer in
-        //        self?.fireDbUsersRef
-        //        .document(userId)
-        //        .addSnapshotListener({ (query, error) in
-        //        guard let error = error else {
-        //        guard let data = query?.data() else { observer.onNext(.failure(.mappingFailed)); return }
-        //        guard let user = User(user: data) else { observer.onNext(.failure(.mappingFailed)); return }
-        //        observer.onNext(.success(user))
-        //        return
-        //        }
-        //        let err = UserServiceError.getError(from: error)
-        //        observer.onNext(.failure(err))
-        //        })
-        //        return Disposables.create()
-        //        })
+        userCollection.document(userId).addSnapshotListener({ (responseData, responseError) in
+            guard let firebaseError = responseError else {
+                guard let data = responseData?.data() else { completion(.failure(.mappingFailed)); return }
+                guard let user = User(user: data) else { completion(.failure(.mappingFailed)); return }
+                completion(.success(user)); return }
+            completion(.failure(UserServiceError.getError(from: firebaseError)))
+        })
     }
 
     func getAll(
         completion: @escaping (Result<[User], UserServiceError>) -> Void
         ) {
-        userCollection.getDocuments { (documents, responseError) in
-            if let users = documents?.documents.compactMap({ docs in
-                docs.data().flatMap({ data in
-                    return User(user: data)
-                })
+        userCollection.getDocuments { (responseData, responseError) in
+            if let users = responseData?.documents.compactMap({ docs in
+                docs.data().flatMap({ data in return User(user: data) })
             }) {
                 users.isEmpty ? completion(.failure(.isEmpty)) : completion(.success(users))
             } else {
                 guard let firebaseError = responseError else { completion(.failure(.notFound)); return }
-                let err = UserServiceError.getError(from: firebaseError)
-                completion(.failure(err))
+                let error = UserServiceError.getError(from: firebaseError)
+                completion(.failure(error))
             }
         }
     }
@@ -122,17 +111,15 @@ class UserService: UserServiceType {
         usersId: [String],
         completion: @escaping (Result<[User], UserServiceError>) -> Void
         ) {
-        userCollection.getDocuments { (documents, error) in
-            if let users = documents?.documents.compactMap({ docs in
-                docs.data().flatMap({ (data)  in
-                    return User(user: data)
-                })
+        userCollection.getDocuments { (responseData, responseError) in
+            if let users = responseData?.documents.compactMap({ docs in
+                docs.data().flatMap({ (data) in return User(user: data) })
             }).filter({ usersId.contains($0.id!) }) {
                 users.isEmpty ? completion(.failure(.isEmpty)) : completion(.success(users))
             } else {
-                guard let error = error else { completion(.failure(.notFound)); return }
-                let err = UserServiceError.getError(from: error)
-                completion(.failure(err))
+                guard let firebaseError = responseError else { completion(.failure(.notFound)); return }
+                let error = UserServiceError.getError(from: firebaseError)
+                completion(.failure(error))
             }
         }
     }
