f282860cada617218e523bf5ae7dd3f952e9ff9f
Yurii Tsymbala <zumbala2013@gmail.com>
2019-02-12 08:07:34 +0200

    Setup View

--- Source/PresentationLayer/Modules/Settings/Controller/SettingsViewController.swift
+++ Source/PresentationLayer/Modules/Settings/Controller/SettingsViewController.swift
@@ -15,6 +15,6 @@ class SettingsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+
 
 }
--- Source/PresentationLayer/Modules/SignUp/View/SignUpControllerView.swift
+++ Source/PresentationLayer/Modules/SignUp/View/SignUpControllerView.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019 Yurii Tsymbala. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class SignUpControllerView: ControllerView {
+
+    // MARK: - Properties
+    // MARK: Callbacks
+
+    var didTouchCreateAccount: EmptyClosure?
+    var didTouchSignIn: EmptyClosure?
+    var doneCallback: (() -> Void)?
+
+    // MARK: Views
+
+
+    // MARK: - UI
+    // MARK: Configuration
+
+    override func configure() {
+        super.configure()
+    }
+}
