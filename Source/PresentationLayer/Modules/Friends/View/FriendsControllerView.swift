701e7f04afdfbe43750f125f71ad04dfd5e46770
Yurii Tsymbala <zumbala2013@gmail.com>
2019-02-12 08:07:55 +0200

    Setup ControllerView

---
+++ Source/PresentationLayer/Modules/Friends/View/FriendsControllerView.swift
@@ -0,0 +1,31 @@
+//
+//  FriendsControllerView.swift
+//  FamillyOrganizer
+//
+//  Created by Yurii Tsymbala on 2/12/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class FriendsControllerView: ControllerView {
+
+    // MARK: - Properties
+    // MARK: Callbacks
+
+    var didTouchCreateAccount: EmptyClosure?
+    var didTouchSignIn: EmptyClosure?
+    var doneCallback: (() -> Void)?
+
+    // MARK: Views
+
+
+    // MARK: - UI
+    // MARK: Configuration
+
+    override func configure() {
+        super.configure()
+    }
+}
+
