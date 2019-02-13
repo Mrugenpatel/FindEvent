c7bf71c77ba0e41b65a6e1539a5fc1432658376d
Yurii Tsymbala <zumbala2013@gmail.com>
2019-02-13 07:40:14 +0200

    Setup Custom Button

--- Source/PresentationLayer/Views/Base/Button.swift
+++ Source/PresentationLayer/Views/Base/Button.swift
@@ -12,22 +12,60 @@ class Button: UIButton {
 
     // MARK: - Properties
 
+    private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+
     var didTouchUpInside: DidTouchUpInside? {
         didSet {
             attachTouchUpInsideEvent()
         }
     }
 
-    var repeatOnLongPress: EmptyClosure? {
+    override var isEnabled: Bool {
         didSet {
-            attachLonPressGestureRecognizer()
+            super.isEnabled = isEnabled
+            if isEnabled {
+                setDefaultView()
+            } else {
+                setDisableView()
+            }
         }
     }
 
-    override var isHighlighted: Bool {
-        didSet {
-            alpha = isHighlighted ? 0.8 : 1
-        }
+    // MARK: - View Setup
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtonView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupButtonView()
+    }
+
+    func setDisableView() {
+        self.backgroundColor = .gray
+    }
+
+    func setDefaultView() {
+        self.backgroundColor = ViewConfig.Colors.blue
+    }
+
+    private func setupButtonView() {
+        layer.cornerRadius = 5
+        backgroundColor = ViewConfig.Colors.blue
+        setTitleColor(ViewConfig.Colors.textWhite, for: .normal)
+
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+        layer.shadowOffset = CGSize(width: 5, height: 5)
+        layer.shadowOpacity = 1.0
+        layer.shadowRadius = 10.0
+
+        contentEdgeInsets = padding
     }
 
 
@@ -38,30 +76,26 @@ class Button: UIButton {
         addTarget(self, action: #selector(touchedUpInside), for: .touchUpInside)
     }
 
-    private func attachLonPressGestureRecognizer() {
-        let gestureRecognizer = UILongPressGestureRecognizer(target: self,
-                                                             action: #selector(handleLongGesture(_:)))
-        addGestureRecognizer(gestureRecognizer)
-    }
-
-    @objc
-    private func handleLongGesture(_ gestureRecognizer: UILongPressGestureRecognizer) {
-        switch gestureRecognizer.state {
-        case .began:
-            timer = Timer.scheduledTimer(withTimeInterval: 0.1,
-                                         repeats: true)
-            { [unowned self] _ in
-                self.repeatOnLongPress?()
-            }
-            timer?.fire()
-        default:
-            timer?.invalidate()
-        }
-
-    }
-    private var timer: Timer?
-
     // MARK: Actions
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        layer.shadowRadius = 20.0
+        transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 6,
+                       options: .allowUserInteraction,
+                       animations: {
+                        self.transform = CGAffineTransform.identity
+        }, completion: nil)
+        super.touchesBegan(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        layer.shadowRadius = 10.0
+        super.touchesEnded(touches, with: event)
+    }
 
     @objc
     private func touchedUpInside() {
